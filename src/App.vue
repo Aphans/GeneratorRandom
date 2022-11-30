7b837055f8ce1c27c6f6e48748162fac29ddb0f6
Aphans <rapperanqeel@hotmail.com>
2022-11-30 12:17:54 +0100

    uploaded

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,14 @@
   <ul >
     <li v-for="(el,key) in fichero" :key="key"> {{ el[0] }}- {{ el[1] }} </li>
   </ul>
-  
 </template>
 
-<script>
+<script setup>
 import ReadFile from "@/components/ReadFile.vue";
-
-export default {
-  components:{
-    ReadFile,
-  },
-  name:"App",
-  data() {
-    return {
-      fichero:[],
-      numeroInCorrecto: false, 
-    }
-  },
-  methods: {
-    cargaFichero(fich) {
+import { ref } from "vue";
+const fichero=ref([]);
+      const numeroInCorrecto=ref(false);
+    const cargaFichero=((fich)=>{
       //Pasamos la cadena de caracteres del fichero obtenido a un array
       let auxiliar =  fich
         .toString()
@@ -30,20 +19,18 @@ export default {
         .split('\n')
       //Si el número de elementos no es par, no podemos continuar
       if (auxiliar.length%2==0) {
-        this.numeroIncorrecto = false;
+        numeroInCorrecto.value = false;
         //Realizamos el emparejamiento aleatorio
-        this.fichero = auxiliar
+        fichero.value = auxiliar
           .sort(()=> Math.random() - 0.5)
           .map((el,idx,arr)=>  (idx % 2 == 1) ? [el,arr[idx-1]]: null)
           .filter( el => el !== null);
       } else {
         //No ha sido un número correcto
-        this.numeroInCorrecto = true;
-        this.fichero=[];
+        numeroInCorrecto.value = true;
+        fichero.value=[];
       };
     }
-        
-  }, 
-};
+);
 </script>
 
--- src/components/ReadFile.vue
+++ src/components/ReadFile.vue
@@ -1,4 +1,5 @@
 <template>
+  {{texto}}
  <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
 <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" 
 id="file_input" 
@@ -6,23 +7,20 @@ type="file"
 @change="loadTextFromFile">
 </template>
   
-<script>
-  export default {
-    name:"ReadFile",
-    props:{
+<script setup>
+  const props =  defineProps({
         texto:{
             type: String,
         },
-    },
-    methods: {
-      loadTextFromFile(ev) {
+      }
+    )
+    const emits = defineEmits(['carga'])
+      const loadTextFromFile=((ev)=>{
         const file = ev.target.files[0];
         const reader = new FileReader();
-        reader.onload = e => this.$emit("carga",e.target.result);
+        reader.onload = e => emits("carga",e.target.result);
         reader.readAsText(file);
       }    
-    },
-    
-  }
+  );
   </script>
   <style></style>
